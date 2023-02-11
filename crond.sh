40c25994ca6bee63068e6e2882723f159c3d48f7
Philip Ullersted <philipullersted@gmail.com>
2023-02-11 20:34:03 +0100

    Revert

--- crond.sh
+++ crond.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "autorestic cron process has started"
-python3 /template.py
 autorestic -c $CRON_CONFIG_DIR --ci cron > /var/log/autorestic-cron.log 2>&1
 echo "autorestic cron process has finished"
