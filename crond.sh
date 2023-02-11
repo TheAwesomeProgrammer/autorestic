9d73b4db48d9be27304bdc4bba00cb85552a795b
Philip Ullersted <philipullersted@gmail.com>
2023-02-11 20:32:29 +0100

    Add template to cron

--- crond.sh
+++ crond.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "autorestic cron process has started"
+python3 /template.py
 autorestic -c $CRON_CONFIG_DIR --ci cron > /var/log/autorestic-cron.log 2>&1
 echo "autorestic cron process has finished"
