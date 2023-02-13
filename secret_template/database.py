5e91022dbf4d8024e16c9231f194120cb2dea4e6
Philip Ullersted <philipullersted@gmail.com>
2023-02-13 12:30:57 +0100

    Fix:

--- secret_template/database.py
+++ secret_template/database.py
@@ -8,4 +8,5 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 if DATABASE_NAME is None or DATABASE_USERNAME is None or DATABASE_PASSWORD is None:
     raise SystemExit(0)
 
-pg_dump('-h', '127.0.0.1', '-U', DATABASE_USERNAME, DATABASE_NAME, '>', '/data/database-dump/dump.sql', _env={"PGPASSWORD": DATABASE_PASSWORD})
+os.makedirs('/data/database-dump/', exist_ok=True)
+pg_dump('-h', '127.0.0.1', '-U', DATABASE_USERNAME, DATABASE_NAME, _out='/data/database-dump/dump.sql', _env={"PGPASSWORD": DATABASE_PASSWORD})
