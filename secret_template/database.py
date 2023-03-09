f23787ecb30ce55d293cb7f71c9d8c5dff0f8bee
Philip Ullersted <philipullersted@gmail.com>
2023-03-09 22:02:39 +0100

    Fix3

--- secret_template/database.py
+++ secret_template/database.py
@@ -15,4 +15,4 @@ if DATABASE_HOST is not None:
     host = DATABASE_HOST
 
 os.makedirs('/data/database-dump/', exist_ok=True)
-pg_dump('-h', '127.0.0.1', '-U', DATABASE_USERNAME, DATABASE_NAME, _out='/data/database-dump/dump.sql', _env={"PGPASSWORD": DATABASE_PASSWORD})
+pg_dump('-h', host, '-U', DATABASE_USERNAME, DATABASE_NAME, _out='/data/database-dump/dump.sql', _env={"PGPASSWORD": DATABASE_PASSWORD})
