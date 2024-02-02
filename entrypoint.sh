c541976323fe81abd18e5cbdfd5a04eac5f4da99
Philip Ullersted <philipullersted@gmail.com>
2024-02-02 15:21:15 +0100

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 python3 /template.py
-autorestic check -c $CRON_CONFIG_DIR
-autorestic $AUTORESTIC_INITIAL_ARGS
 if [ -n "$CRON_CONFIG_DIR" ]
 then
+    autorestic check -c $CRON_CONFIG_DIR
+    autorestic $AUTORESTIC_INITIAL_ARGS
     echo "CRON_CONFIG_DIR is set, cron enabled"
     crond -f -L /var/log/cron.log
 else
+    autorestic check -c $CONFIG_DIR
+    autorestic $AUTORESTIC_INITIAL_ARGS
     exit 0
 fi
